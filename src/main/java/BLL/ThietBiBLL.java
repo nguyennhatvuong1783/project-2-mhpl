c7b35f1494e602594ecf7f73c9170fc58b07af99
Nguyen Nhat Vuong <king178vn@gmail.com>
2024-03-25 14:04:17 +0700

    Create BLL, DAL, GUI packages and add file sql

---
+++ src/main/java/BLL/ThietBiBLL.java
@@ -0,0 +1,14 @@
+package BLL;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nguyen Nhat Vuong
+ */
+public class ThietBiBLL {
+    
+}
