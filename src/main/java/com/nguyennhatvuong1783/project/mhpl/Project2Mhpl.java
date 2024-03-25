ce87eeeb843806e614a6b6cce97c4ca3f21e4994
Nguyen Nhat Vuong <king178vn@gmail.com>
2024-03-25 13:45:15 +0700

    First commit

---
+++ src/main/java/com/nguyennhatvuong1783/project/mhpl/Project2Mhpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nguyennhatvuong1783.project.mhpl;
+
+/**
+ *
+ * @author Nguyen Nhat Vuong
+ */
+public class Project2Mhpl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
