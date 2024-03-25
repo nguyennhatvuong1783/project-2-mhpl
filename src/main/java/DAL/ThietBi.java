68dbd929408f856cdda17ee0ae770f3fe08aa7f0
tranqui268 <trannhatqui712@gmail.com>
2024-03-25 22:09:10 +0700

    test: hibernate.cfg.xml build success

--- src/main/java/DAL/ThietBi.java
+++ src/main/java/DAL/ThietBi.java
@@ -4,10 +4,65 @@
  */
 package DAL;
 
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Data;
+
 /**
  *
  * @author Nguyen Nhat Vuong
  */
-public class ThietBi {
+@Entity
+@Table (name = "thietbi")
+public class ThietBi implements Serializable{
+    @Id
+    private int MaTB;
+    
+    @Column
+    private String TenTB;
+    
+    @Column
+    private String MoTaTB;
+
+    public ThietBi() {
+    }
+
+    public ThietBi(int MaTB, String TenTB, String MoTaTB) {
+        this.MaTB = MaTB;
+        this.TenTB = TenTB;
+        this.MoTaTB = MoTaTB;
+    }
+    
+    
+
+    public int getMaTB() {
+        return MaTB;
+    }
+
+    public void setMaTB(int MaTB) {
+        this.MaTB = MaTB;
+    }
+
+    public String getTenTB() {
+        return TenTB;
+    }
+
+    public void setTenTB(String tenTB) {
+        this.TenTB = tenTB;
+    }
+
+    public String getMoTaTB() {
+        return MoTaTB;
+    }
+
+    public void setMoTaTB(String MoTaTB) {
+        this.MoTaTB = MoTaTB;
+    }
+    
+    
+    
+    
+            
+    
     
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,13 @@
+
+import DAL.ThietBi;
+import java.util.List;
+import java.util.Iterator;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,7 +18,37 @@
  * @author Nguyen Nhat Vuong
  */
 public class Main {
+    private static SessionFactory factory;
     public static void main(String[] args) {
-        
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (HibernateException e) {
+            System.err.println("Failed to create sessionFactory object." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+        Main m = new Main();
+        m.listThietBi();
     }
+    
+    public void listThietBi(){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List thietBi = session.createQuery("FROM ThietBi").list();
+            for(Iterator iterator = thietBi.iterator(); iterator.hasNext();){
+                ThietBi tb = (ThietBi) iterator.next();
+                System.out.println(" MaTB: " + tb.getMaTB());
+                System.out.println(" TenTB: " + tb.getTenTB());
+                System.out.println(" MoTaTB: " + tb.getMoTaTB());
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if(tx!=null) tx.rollback();
+        } finally {
+            session.close();
+        }
+    }
+        
 }
