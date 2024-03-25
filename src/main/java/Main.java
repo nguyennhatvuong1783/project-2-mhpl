002f49d843e00409557538f05e39ad6dd3e31459
Nguyen Nhat Vuong <king178vn@gmail.com>
2024-03-25 13:50:19 +0700

    setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.nguyennhatvuong1783.project.mhpl;
 
 /**
  *
  * @author Nguyen Nhat Vuong
  */
-public class Project2Mhpl {
-
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
